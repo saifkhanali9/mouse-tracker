b60fc414b56482ba24cb12ebf30d10ac688850d4
Saif Ali Khan <saifkhanali.9@gmail.com>
2020-06-25 14:04:49 +0200

    Multithreading added

--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,24 @@ using Newtonsoft.Json;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows;
+using System.Threading;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        Timer timer = new Timer();
+        long time1 = 0;
+        int lastx1 = 0;
+        int lasty1 = 0;
+        long lastTime1 = 0;
+        double vx = 0;
+        double vy = 0;
+        double v = 0;
+        int vMag = 0;
+        int timeDiff = 0;
+        int xDiff = 0;
+        int yDiff = 0;
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
         System.Drawing.Point lastPoint = System.Drawing.Point.Empty;//Point.Empty represents null for a Point object
         int check = 0;
         int clickCheck = 0;
@@ -37,19 +49,36 @@ namespace WindowsFormsApp1
         }
         private void timer_Tick(object sender, EventArgs e)
         {
-
-            firstClick();
+            Thread th = new Thread(firstClick);
+            th.Start();
+            //firstClick();
             ScreenElement structure = ScreenElement.fromAutomationElement(ElementFromCursor());
             structureInCondition = structure;
             long currTime = new StartTime().getStartTime();
             long timeSinceStartSession = currTime - startTime;
             System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mm\"" + ", \"mouseX\": " + Cursor.Position.X + ", \"mouseY\": " + Cursor.Position.Y+ ", \"time\": " + timeSinceStartSession + "}");
         }
-
+        private double clip(double a, double min = 0, double max = 1)
+        {
+            return Math.Max(min, Math.Min(max, a));
+        }
+        private Color interpolate(Color a, Color b, double ratio)
+        {
+            ratio = clip(ratio);
+            return Color.FromArgb(128, (int)((b.R - a.R) * ratio + a.R), (int)((b.G - a.G) * ratio + a.G), (int)((b.B - a.B) * ratio + a.B));
+        }
+        private Color interpolate(List<Color> colors, double ratio)
+        {
+            ratio *= colors.Count;
+            var lower = (int)clip(ratio, 0, colors.Count - 2);
+            var upper = lower + 1;
+            return interpolate(colors[lower], colors[upper], ratio - lower);
+        }
         private void firstClick()
         {
             if (check == 0)
             {
+                lastTime1 = new StartTime().getStartTime();
                 lastPoint = Cursor.Position;
                 //isMouseDown = true;
                 check = 1;
@@ -73,15 +102,26 @@ namespace WindowsFormsApp1
                 {//we need to create a Graphics object to draw on the picture box, its our main tool
 
                     //when making a Pen object, you can just give it color only or give it color and pen size
-
-                    g.DrawLine(new Pen(Color.Black, 1), lastPoint, Cursor.Position);
+                    time1 = new StartTime().getStartTime();
+                    xDiff = Math.Abs(Cursor.Position.X - lastPoint.X);
+                    timeDiff = Convert.ToInt32(time1 - lastTime1);
+                    yDiff = Math.Abs(Cursor.Position.Y - lastPoint.Y);
+                    vx = (xDiff * 1.0) / timeDiff;
+                    vy = (yDiff * 1.0) / timeDiff;
+                    v = Math.Sqrt((vx * vx) + (vy * vy));
+                    vMag = Convert.ToInt32(v);
+                    double r = v / 11;
+                    Color color = interpolate(new List<Color>() { Color.Black, Color.Black, Color.FromArgb(0x330000), Color.FromArgb(0x660000), Color.FromArgb(0x660000), Color.FromArgb(0x800000), Color.FromArgb(0x8B0000), Color.FromArgb(0xCD0000), Color.FromArgb(0xEE0000), Color.FromArgb(0xFF0000), Color.FromArgb(0xFE0000), Color.FromArgb(0xFF2B2B),
+                        Color.FromArgb(0xFF3030), Color.FromArgb(0xFF3333), Color.FromArgb(0xFF4040), Color.FromArgb(0xFF6666), Color.FromArgb(0xFF6A6A), Color.FromArgb(0xFF7777), Color.FromArgb(0xFF9090), Color.FromArgb(0xFF9393), Color.FromArgb(0xFF9999), Color.FromArgb(0xFFA9A9), Color.FromArgb(0xFFAAAA), Color.FromArgb(0xFFAEAE), Color.FromArgb(0xFFBBBB), Color.FromArgb(0xFFC1C1), Color.FromArgb(0xFFCCCC), Color.FromArgb(0xFFD5D5), Color.FromArgb(0xFFDDDD)}, r);
+                    //Color color = Color.Black;
+                    g.DrawLine(new Pen(color, 1), lastPoint.X, lastPoint.Y, Cursor.Position.X, Cursor.Position.Y);
                     g.SmoothingMode = SmoothingMode.AntiAlias;
                     //this is to give the drawing a more smoother, less sharper look
 
                 }
 
                 pictureBox1.Invalidate();//refreshes the picturebox
-
+                lastTime1 = time1;
                 lastPoint = Cursor.Position;//keep assigning the lastPoint to the current mouse position
 
             }
