e9e5741c61b1b55f9d1fe6d16c0334b9e8ef6b54
Saif Ali Khan <saifkhanali.9@gmail.com>
2020-08-11 16:49:33 +0200

    Confirmation push

--- Form1.cs
+++ Form1.cs
@@ -56,7 +56,7 @@ namespace WindowsFormsApp1
             structureInCondition = structure;
             long currTime = new StartTime().getStartTime();
             long timeSinceStartSession = currTime - startTime;
-            System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mm\"" + ", \"mouseX\": " + Cursor.Position.X + ", \"mouseY\": " + Cursor.Position.Y+ ", \"time\": " + timeSinceStartSession + "}");
+            System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mm\"" + ", \"mouseX\": " + Cursor.Position.X + ", \"mouseY\": " + Cursor.Position.Y+ ", \"time\": " + timeSinceStartSession + ", \"relative\": true" + "}");
         }
         private double clip(double a, double min = 0, double max = 1)
         {
@@ -111,10 +111,19 @@ namespace WindowsFormsApp1
                     v = Math.Sqrt((vx * vx) + (vy * vy));
                     vMag = Convert.ToInt32(v);
                     double r = v / 11;
-                    Color color = interpolate(new List<Color>() { Color.Black, Color.Black, Color.FromArgb(0x330000), Color.FromArgb(0x660000), Color.FromArgb(0x660000), Color.FromArgb(0x800000), Color.FromArgb(0x8B0000), Color.FromArgb(0xCD0000), Color.FromArgb(0xEE0000), Color.FromArgb(0xFF0000), Color.FromArgb(0xFE0000), Color.FromArgb(0xFF2B2B),
-                        Color.FromArgb(0xFF3030), Color.FromArgb(0xFF3333), Color.FromArgb(0xFF4040), Color.FromArgb(0xFF6666), Color.FromArgb(0xFF6A6A), Color.FromArgb(0xFF7777), Color.FromArgb(0xFF9090), Color.FromArgb(0xFF9393), Color.FromArgb(0xFF9999), Color.FromArgb(0xFFA9A9), Color.FromArgb(0xFFAAAA), Color.FromArgb(0xFFAEAE), Color.FromArgb(0xFFBBBB), Color.FromArgb(0xFFC1C1), Color.FromArgb(0xFFCCCC), Color.FromArgb(0xFFD5D5), Color.FromArgb(0xFFDDDD)}, r);
-                    //Color color = Color.Black;
-                    g.DrawLine(new Pen(color, 1), lastPoint.X, lastPoint.Y, Cursor.Position.X, Cursor.Position.Y);
+                    if (v == 0)
+                    {
+                        g.DrawRectangle(new Pen(Color.LightBlue), Cursor.Position.X, Cursor.Position.Y, 2, 2);
+                    }
+                    else
+                    {
+                        //Color color = interpolate(new List<Color>() { Color.Black, Color.Black, Color.FromArgb(0x330000), Color.FromArgb(0x660000), Color.FromArgb(0x660000), Color.FromArgb(0x800000), Color.FromArgb(0x8B0000), Color.FromArgb(0xCD0000), Color.FromArgb(0xEE0000), Color.FromArgb(0xFF0000), Color.FromArgb(0xFE0000), Color.FromArgb(0xFF2B2B),
+                        //Color.FromArgb(0xFF3030), Color.FromArgb(0xFF3333), Color.FromArgb(0xFF4040), Color.FromArgb(0xFF6666), Color.FromArgb(0xFF6A6A), Color.FromArgb(0xFF7777), Color.FromArgb(0xFF9090), Color.FromArgb(0xFF9393), Color.FromArgb(0xFF9999), Color.FromArgb(0xFFA9A9), Color.FromArgb(0xFFAAAA), Color.FromArgb(0xFFAEAE), Color.FromArgb(0xFFBBBB), Color.FromArgb(0xFFC1C1), Color.FromArgb(0xFFCCCC), Color.FromArgb(0xFFD5D5), Color.FromArgb(0xFFDDDD)}, r);
+                        //Color color = interpolate(new List<Color>() { Color.FromArgb(0x0500ff), Color.FromArgb(0x0400ff), Color.FromArgb(0x0300ff), Color.FromArgb(0x0200ff), Color.FromArgb(0x0100ff), Color.FromArgb(0x0000ff), Color.FromArgb(0x0002ff), Color.FromArgb(0x0012ff), Color.FromArgb(0x0022ff), Color.FromArgb(0x0032ff), Color.FromArgb(0x0044ff), Color.FromArgb(0x0054ff), Color.FromArgb(0x0064ff), Color.FromArgb(0x0074ff), Color.FromArgb(0x0084ff), Color.FromArgb(0x0094ff), Color.FromArgb(0x00a4ff), Color.FromArgb(0x00b4ff), Color.FromArgb(0x00c4ff), Color.FromArgb(0x00d4ff), Color.FromArgb(0x00e4ff), Color.FromArgb(0x00fff4), Color.FromArgb(0x00ffd0), Color.FromArgb(0x00ffa8), Color.FromArgb(0x00ff83), Color.FromArgb(0x00ff5c), Color.FromArgb(0x00ff36), Color.FromArgb(0x00ff10), Color.FromArgb(0x17ff00), Color.FromArgb(0x3eff00), Color.FromArgb(0x65ff00), Color.FromArgb(0x8aff00), Color.FromArgb(0xb0ff00), Color.FromArgb(0xd7ff00), Color.FromArgb(0xfdff00), Color.FromArgb(0xFFfa00), Color.FromArgb(0xFFf000), Color.FromArgb(0xFFe600), Color.FromArgb(0xFFdc00), Color.FromArgb(0xFFd200), Color.FromArgb(0xFFc800), Color.FromArgb(0xFFbe00), Color.FromArgb(0xFFb400), Color.FromArgb(0xFFaa00), Color.FromArgb(0xFFa000), Color.FromArgb(0xFF9600), Color.FromArgb(0xFF8c00), Color.FromArgb(0xFF8200), Color.FromArgb(0xFF7800), Color.FromArgb(0xFF6e00), Color.FromArgb(0xFF6400), Color.FromArgb(0xFF5a00), Color.FromArgb(0xFF5000), Color.FromArgb(0xFF4600), Color.FromArgb(0xFF3c00), Color.FromArgb(0xFF3200), Color.FromArgb(0xFF2800), Color.FromArgb(0xFF1e00), Color.FromArgb(0xFF1400), Color.FromArgb(0xFF0a00), Color.FromArgb(0xFF0000) }, r);
+                        Color color = interpolate(new List<Color>() { Color.FromArgb(0x0500ff), Color.FromArgb(0x0000ff), Color.FromArgb(0x00fff4), Color.FromArgb(0xfdff00), Color.FromArgb(0xFF5000), Color.FromArgb(0xFF4600), Color.FromArgb(0xFF0000) }, r);
+                        //Color color = Color.Black;
+                        g.DrawLine(new Pen(color, 2), lastPoint.X, lastPoint.Y, Cursor.Position.X, Cursor.Position.Y);
+                    }
                     g.SmoothingMode = SmoothingMode.AntiAlias;
                     //this is to give the drawing a more smoother, less sharper look
 
@@ -155,11 +164,12 @@ namespace WindowsFormsApp1
         static extern bool GetCursorPos(out POINT lpPoint);
         private void Form1_Load(object sender, EventArgs e)
         {
+            pictureBox1.BackColor = Color.DarkGray;
             Hook.GlobalEvents().MouseClick += MouseClickAll;
             long currTime = new StartTime().getStartTime();
             long timeSinceStartSession = currTime - startTime;
             ScreenElement structure = ScreenElement.fromAutomationElement(ElementFromCursor());
-            System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "{\"eventType\": \"mm\"" + ", \"mouseX\": " + Cursor.Position.X + ", \"mouseY\": " + Cursor.Position.Y + ", \"time\": " + timeSinceStartSession + "}");
+            System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "{\"eventType\": \"mm\"" + ", \"mouseX\": " + Cursor.Position.X + ", \"mouseY\": " + Cursor.Position.Y + ", \"time\": " + timeSinceStartSession + ", \"relative\": true" + "}");
         }
 
         private void MouseClickAll(object sender, MouseEventArgs e)
@@ -171,11 +181,19 @@ namespace WindowsFormsApp1
                 long timeSinceStartSession = currTime - startTime;
                 if (double.IsInfinity(structure.x) || double.IsInfinity(structure.y))
                 {
-                    System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mc\"" + ", \"mouseX\": " + p.X + ", \"mouseY\": " + p.Y + ", \"elementX\": " + structureInCondition.x + ", \"elementY\": " + structureInCondition.y + ", \"elementW\": " + structureInCondition.width + ", \"elementH\": " + structureInCondition.height + ", \"time\": " + timeSinceStartSession + "}");
+                    using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                    {
+                        g.DrawEllipse(new Pen(Color.Green), p.X, p.Y, 4, 4);
+                    }
+                    System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"md\"" + ", \"mouseX\": " + p.X + ", \"mouseY\": " + p.Y + ", \"elementX\": " + structureInCondition.x + ", \"elementY\": " + structureInCondition.y + ", \"elementW\": " + structureInCondition.width + ", \"elementH\": " + structureInCondition.height + ", \"time\": " + timeSinceStartSession + ", \"relative\": true" + "}");
                 }
                 else
                 {
-                    System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mc\"" + ", \"mouseX\": " + p.X + ", \"mouseY\": " + p.Y + ", \"elementX\": " + structure.x + ", \"elementY\": " + structure.y + ", \"elementW\": " + structure.width + ", \"elementH\": " + structure.height + ", \"time\": " + timeSinceStartSession + "}");
+                    using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                    {
+                        g.DrawEllipse(new Pen(Color.Green), p.X, p.Y, 4, 4);
+                    }
+                    System.IO.File.AppendAllText(@"C:\Saif\Office\C#\Projects\MouseClick\file\" + startTime.ToString() + ".json", "\n{\"eventType\": \"mc\"" + ", \"mouseX\": " + p.X + ", \"mouseY\": " + p.Y + ", \"elementX\": " + structure.x + ", \"elementY\": " + structure.y + ", \"elementW\": " + structure.width + ", \"elementH\": " + structure.height + ", \"time\": " + timeSinceStartSession + ", \"relative\": true" + "}");
                 }
             }
         }
